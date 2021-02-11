073f8b44a31e1f491a57ee0359365598727dd1ee
Mahesh Vuppala <mahesh.vuppala@icicletech.com>
2021-02-11 17:05:19 +0530

    Before integrating Twitter account

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+#email: string
+#password_digest: string
+
+class User < ApplicationRecord
+  has_secure_password
+  
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+end
